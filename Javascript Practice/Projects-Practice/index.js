cc0b9eca09979b0c6621a6a36a197f0b157e16d9
saurabhgit2 <saurabhgithub0@gmail.com>
2026-06-12 14:19:53 +1200

    Added Event Listener and functionality to button to switch from You clicked me to click me

--- Javascript Practice/Projects-Practice/index.js
+++ Javascript Practice/Projects-Practice/index.js
@@ -1,7 +1,22 @@
 console.log("Creating a Weather App");
-const button1=document.getElementById("btn")
+const button1=document.getElementById("btn");
+const body1= document.getElementsByTagName("body");
 function changeBtn(){
-    button1.innerText="You Clicked me.";
+    if (button1.innerText=="Click me"){
+        button1.innerText="You Clicked me.";
+        button1.style.background="#f59a9a";
+    }
+    else if (button1.innerText=="You Clicked me."){
+        button1.innerText="Click me";
+        button1.style.background="blanchedalmond";
+    }
+    else
+    {
+                button1.innerText="You Clicked me.";
+        button1.style.background="#f59a9a";
+    }
+    
+    
 }
 
 button1.addEventListener("click",changeBtn);
