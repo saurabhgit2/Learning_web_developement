1486abfa583afef9909359406234f2684f7bfac6
saurabhgit2 <saurabhgithub0@gmail.com>
2026-06-12 14:27:50 +1200

    Added background Div to button

--- Javascript Practice/Projects-Practice/index.js
+++ Javascript Practice/Projects-Practice/index.js
@@ -1,10 +1,11 @@
 console.log("Creating a Weather App");
 const button1=document.getElementById("btn");
-const body1= document.getElementsByTagName("body");
 function changeBtn(){
     if (button1.innerText=="Click me"){
         button1.innerText="You Clicked me.";
         button1.style.background="#f59a9a";
+        
+        
     }
     else if (button1.innerText=="You Clicked me."){
         button1.innerText="Click me";
@@ -12,7 +13,7 @@ function changeBtn(){
     }
     else
     {
-                button1.innerText="You Clicked me.";
+        button1.innerText="You Clicked me.";
         button1.style.background="#f59a9a";
     }
     
