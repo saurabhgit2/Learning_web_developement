eb95e64347cc33255c3a7364bd68aa769acc7649
saurabhgit2 <saurabhgithub0@gmail.com>
2026-06-30 10:32:30 +1200

    Used Constructor to create Object

--- Javascript Practice/Topics/Objects/index.js
+++ Javascript Practice/Topics/Objects/index.js
@@ -9,16 +9,39 @@
 // };
 
 
-//Factory function to create 
+//Factory function to create Objects
 
-function createRectangle () {
-    return rectangle={
-    length:1,
-    breadth:2,
-    draw: function(){
-       console.log("This is object's method.");
-    }
+// function createRectangle (l,b) {
+//     return rectangle={
+//     length:l,
+//     breadth:b,
+//     draw: function(){
+//        console.log("This is object's method.");
+//     }
+    
+// };
+
+// }
+
+// let rectangle1=createRectangle(4,5)
+
+// Using Constructor Function to create Objects (Use PascalNotation)
+
+function CreateRectangle(){
+    // this is used to refer the object, here, it is referring to an empty object
+    this.length=1,
+    this.breadth=4,
+    this.draw=function(){console.log('Drawing rectangle')};
 };
-}
 
-let rectangle1=createRectangle()
+// new creates an empty object
+let rectangleObject=new CreateRectangle();
+
+// Dynamic property
+// Adding property:value
+rectangleObject.color='red';
+console.log(rectangleObject)
+
+// deleting property
+delete rectangleObject.color;
+console.log(rectangleObject);
